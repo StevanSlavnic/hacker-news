85277ee624cbba9e97a9697953e22214496347a8
Stevan <slavnic.stevan@gmail.com>
2019-03-07 22:04:52 +0100

    Responsive polishment of Grid list Header and Stories, added mixin for media queries

--- src/containers/News/News.js
+++ src/containers/News/News.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { itemsFetchData } from '../../store/actions/itemsAction'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { itemsFetchData } from "../../store/actions/itemsAction";
 
-import SingleStory from '../SingleStory/SingleStory'
-import GridList from '../../components/GridList/GridList'
-import classes from './News.module.scss'
+import SingleStory from "../SingleStory/SingleStory";
+import GridList from "../../components/GridList/GridList";
+import classes from "./News.module.scss";
 
 class News extends Component {
-  state = {}
+  state = {};
 
-  componentDidMount () {
-    window.scrollTo(0, 0)
+  componentDidMount() {
+    window.scrollTo(0, 0);
     this.props.fetchData(
-      'https://hacker-news.firebaseio.com/v0/newstories.json'
-    )
+      "https://hacker-news.firebaseio.com/v0/newstories.json"
+    );
   }
 
-  render () {
-    const newStories = this.props.items.items ? this.props.items.items : []
+  render() {
+    const newStories = this.props.items.items ? this.props.items.items : [];
 
     const storyRender = () =>
-      newStories.slice(0, 50).map(function (item) {
+      newStories.slice(0, 50).map(function(item) {
         return (
           <div key={item}>
             <SingleStory>{item}</SingleStory>
           </div>
-        )
-      })
+        );
+      });
     return (
       <div className={classes.NewsWrapper}>
         <GridList className={classes.GridList}>{storyRender()}</GridList>
       </div>
-    )
+    );
   }
 }
 
@@ -39,16 +39,16 @@ const mapStateToProps = state => {
   return {
     items: state.items,
     isLoading: state.itemsIsLoading
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
     fetchData: url => dispatch(itemsFetchData(url))
-  }
-}
+  };
+};
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(News)
+)(News);
